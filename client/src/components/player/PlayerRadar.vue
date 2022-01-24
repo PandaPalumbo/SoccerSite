680296a5deeb6a909c5be6b51db82882ca0191f4
PandaPalumbo <mattdpalumbo@gmail.com>
2022-01-24 16:14:56 -0500

    Cleaning up Radar styling

--- client/src/components/player/PlayerRadar.vue
+++ client/src/components/player/PlayerRadar.vue
@@ -1,69 +1,77 @@
 <template>
-  <b-row>
-    <b-col cols="4">
-      <b-form-checkbox-group @change="generateGraph()" v-model="selectedFields">
-        <b-row>
-          <div v-for="(category, i) in Object.keys(categories)" :key="i">
-            <b-col cols="5">
-              <h4>{{ category }}</h4>
-              <div v-for="(field, i) in categories[category]" :key="i">
-                <b-form-checkbox v-if="category !== 'game'" :value="`${category}_${field}`">{{
-                    field
+  <b-container fluid>
+<!--    CATEGORIES / FIELDS-->
+    <b-row>
+      <b-col cols="4">
+        <b-row class="">
+          <b-col v-for="(category, i) in Object.keys(categories)" :key="i" cols="5 p-2" class="border rounded m-1">
+            <b-form-checkbox-group v-if="category !== 'game'" @change="generateGraph()" v-model="selectedFields">
+              <section v-if="category !== 'game'">
+                <h4>{{ prettyCasing(category) }}</h4>
+                <b-form-checkbox v-for="(field, i) in categories[category]" :key="i" :value="`${category}_${field}`">{{
+                    prettyCasing(field)
                   }}
                 </b-form-checkbox>
-              </div>
-            </b-col>
-          </div>
-        </b-row>
-      </b-form-checkbox-group>
-
-      <b-form-radio-group @change="generateGraph()" v-model="selectedTimeField">
-        <b-row>
-          <b-col cols="5">
-            <h4>Time Measures</h4>
-            <div v-for="(category, i) in timeMeasureCategories.games" :key="i">
-
-
-              <b-form-radio :value="`${category}`">{{ category }}</b-form-radio>
-
-            </div>
+              </section>
+            </b-form-checkbox-group>
           </b-col>
         </b-row>
-      </b-form-radio-group>
-    </b-col>
+<!--        TIME MEASURES-->
+        <b-row>
+          <b-form-radio-group @change="generateGraph()" v-model="selectedTimeField">
 
-    <b-col cols="8">
-      <div v-if="selectedFields.length >= 3 && radarData">
-        <Radar style="width:65%; height: 65%;" :chart-data="radarData" :options="radarOptions"/>
-      </div>
-      <div class="text-center m-auto" v-else>
-        <h3 class="m-auto">Select 3 or more stats to compare with a radar chart!</h3>
-      </div>
-    </b-col>
-  </b-row>
+            <b-col cols="12" class="border rounded m-1">
+              <h4>Time Measures</h4>
+              <div v-for="(category, i) in timeMeasureCategories.games" :key="i">
+
+
+                <b-form-radio :value="`${category}`">{{ category }}</b-form-radio>
+
+              </div>
+            </b-col>
+          </b-form-radio-group>
+        </b-row>
+      </b-col>
+
+      <b-col cols="8">
+        <div class="m-auto text-center"  v-if="selectedFields.length >= 3 && radarData && !isGenerating">
+          <h3>
+            {{this.selectedTimeField === 'minutes' ? 'Per 90' : 'Per appearance'}}
+          </h3>
+        </div>
+        <div  class="text-center m-auto" v-if="selectedFields.length >= 3 && radarData && !isGenerating">
+          <Radar style="width:65%; height: 65%; margin: auto" :chart-data="radarData" :options="radarOptions"/>
+        </div>
+        <div class="text-center m-auto" v-else>
+          <h3 class="m-auto">Select 3 or more stats to compare with a radar chart!</h3>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+
 </template>
 
 <script>
 import Radar from "../charts/Radar";
 import {mapState} from "vuex";
-import {getColor} from "@/utils";
+import {getColor, prettyCasing} from "@/utils";
 
 export default {
   name: "PlayerRadar",
   components: {Radar},
-  props: {
-  },
+  props: {},
   data() {
     return {
       selectedFields: [],
       selectedTimeField: 'minutes',
+      isGenerating: false,
       radarData: null,
       radarOptions: //options
           {
             legend: {
               labels: {
                 fontColor: '#f7f7f7',
-                fontSize: 16,
+                fontSize: 15,
               }
             },
             scale: {
@@ -116,7 +124,7 @@ export default {
           let category = selected.split('_')[0]
           let field = selected.split('_')[1]
           let timeMath = this.selectedTimeField == 'minutes' ? statistics['games'][this.selectedTimeField] / 90 : statistics['games'][this.selectedTimeField]
-          ret[selected] = ( statistics[category][field] / timeMath ).toFixed(2)
+          ret[selected] = (statistics[category][field] / timeMath).toFixed(2)
         })
         return ret
       })
@@ -131,8 +139,12 @@ export default {
   methods: {
     generateGraph() {
       if (this.selectedFields.length >= 3) {
+        this.isGenerating = true
+        let labels = this.selectedFields.map(field => {
+          return prettyCasing(field)
+        })
         this.radarData = {
-          labels: this.selectedFields,
+          labels: labels,
           datasets: null,
         };
         let dataset = this.radarStats.map(stat => {
@@ -143,11 +155,7 @@ export default {
             obj['data'].push(parseFloat(stat[field]));
           })
           obj['backgroundColor'] = color[0];
-          if(this.selectedTimeField == 'minutes'){
-            obj['label'] = stat.name + ', per 90'
-          } else {
-            obj['label'] = stat.name + ', per appearance'
-          }
+          obj['label'] = stat.name
           obj['pointBackgroundColor'] = color[1]
           obj['lineTension'] = 0
           obj['borderColor'] = color[1]
@@ -155,10 +163,33 @@ export default {
           return obj;
         })
         console.log('Datasets: ', dataset)
+        this.radarOptions = {
+          legend: {
+            labels: {
+              fontColor: '#f7f7f7',
+              fontSize: 15,
+            }
+          },
+          scale: {
+            gridLines: {
+              color: '#f7f7f7',
+            },
+            pointLabels: {
+              fontColor: '#f7f7f7',
+              fontSize: 12,
+            }
+          },
+          maintainAspectRatio: true,
+          animation: {
+            animateRotate: true
+          }
+        }
         this.radarData.datasets = dataset;
+        this.isGenerating = false
         console.log(this.radarData)
       }
     },
+    prettyCasing
   }
 }
 </script>
